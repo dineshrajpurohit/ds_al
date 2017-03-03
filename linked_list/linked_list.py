f31e273d78387e8cf57cad5a468826c548d55c91
Dinesh Purohit <dpurohit@ucop.edu>
2017-03-02 18:46:28 -0800

    Adding insert functionality: Adding to front or back of the linked list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -10,4 +10,45 @@ class Node:
         return "{} ---> {}".format(self.item, self.next)
 
 
+class Linked_List:
 
+    def __init__(self):
+        self.root = None
+        self.count = 0
+
+    def size(self):
+        return self.count
+
+    # Insertions
+    def insert_front(self, node):
+        if self.root is None:
+            self.root = node
+        else:
+            node.next = self.root
+            self.root = node
+        self.count += 1
+
+    def insert_end(self, node):
+        pointer = self.root
+        if pointer is None:
+            self.root = node
+        while pointer.next  is not None:
+            pointer = pointer.next
+        pointer.next = node
+        self.count += 1
+
+    def __repr__(self):
+        return "Total Nodes: {} Root: {}".format(self.count, self.root)
+
+
+ll = Linked_List()
+print ll
+node1 = Node("A", None)
+ll.insert_front(node1)
+print ll
+node2 = Node("B")
+ll.insert_front(node2)
+print ll
+node3 = Node("C")
+ll.insert_end(node3)
+print ll
