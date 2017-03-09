f534eaa7b3be43c9d1ae52dd95cf7e22cd03029a
Dinesh Purohit <dpurohit@ucop.edu>
2017-03-09 10:03:01 -0800

    Implementation of bag using linked list

--- bag.py
+++ bag.py
@@ -1,17 +1,33 @@
-from linked_list import Linked_List
+from linked_list import Linked_List, Node
+import random
 
 class Bag(Linked_List):
 
     def __init__(self):
         Linked_List.__init__(self)
 
-    def add(self):
-        pass
+    def add(self, item):
+        """Stores in a random location in the Linked list"""
+        if self.count == 0:
+            random_location = 0
+        else:
+            random_location = random.randint(0, self.count-1)
+        self.insert(Node(item), random_location)
 
     def is_empty(self):
-        pass
+        return self.count == 0
 
     def size(self):
-        pass
+        return self.count
 
 
+print "\n\nBag implementation"
+bag = Bag()
+print "Is Bag empty? ", bag.is_empty()
+bag.add("A")
+bag.add("B")
+bag.add("C")
+bag.add("X")
+print "Is Bag empty? ", bag.is_empty()
+print bag.size()
+print bag
