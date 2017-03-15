b6cc85a0b283ae2799bb7e37b9fd0ce778865fe4
Dinesh Purohit <dpurohit@ucop.edu>
2017-03-15 10:24:17 -0700

    Added exercise 1.3.5: Binary conversion also extended to be general base conversion

--- stack_examples.py
+++ stack_examples.py
@@ -31,6 +31,29 @@ def check_balanced_parantheses(par_str):
         return False
 
 
+def decimal_to_binary(num):
+    """ Exercise: 1.3.5 The code given in the exercise is the logic to print binary representation of the decimal number"""
+    stack = Stack()
+    while num > 0:
+        stack.push(num % 2)
+        num = num / 2
+    binary = ""
+    while not stack.is_empty():
+        binary += str(stack.pop())
+    return binary
+
+def base_conversion(num, base):
+    """Extending 1.3.5: More general base conversion. Converts Decimal to Binary, Decimal to Octal and Decimal to Hex"""
+    stack = Stack()
+    num_str = "0123456789ABCDEF"
+    while num > 0:
+        stack.push(num % base)
+        num = num / base
+    conversion = ""
+    while not stack.is_empty():
+        conversion += num_str[stack.pop()]
+    return conversion
+
 
 def test_stack_examples():
     print "Output string from stack (reverser)"
@@ -42,6 +65,13 @@ def test_stack_examples():
     print "Valid Paranthesis:", check_balanced_parantheses("{{([][])}()}")
     print "Valid Paranthesis: ", check_balanced_parantheses("")
     print "Invalid Paranthesis", check_balanced_parantheses("[{()]")
+    print "\n"
+    print "Binary representation of 50: ", decimal_to_binary(50)
+    print "Binary representation of 275", decimal_to_binary(275)
+    print "\n"
+    print "Base conversion: To Binary (275): ", base_conversion(233, 2)
+    print "Base conversion: To Oct (275): ", base_conversion(233, 8)
+    print "Base conversion: To Hex (275): ", base_conversion(233, 16)
 
 if __name__ == '__main__':
     test_stack_examples()
